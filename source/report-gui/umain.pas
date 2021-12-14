a130c196db27278097c95ebfca0a94d2733efb17
Shidel <shidel@users.noreply.github.com>
2021-12-14 17:00:25 -0500

    devel

---
+++ source/report-gui/umain.pas
@@ -0,0 +1,28 @@
+unit uMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+{$I version.inc}
+
+type
+  TfMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  fMain: TfMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
