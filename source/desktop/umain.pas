d9d03afc2142f52d46e0c91b5ed9601738a62a28
Shidel <shidel@users.noreply.github.com>
2022-02-21 05:28:29 -0500

    devel

---
+++ source/desktop/umain.pas
@@ -0,0 +1,28 @@
+unit uMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+{$I version.inc}
+
+type
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
